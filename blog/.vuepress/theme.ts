45496509184b87312c4bb8cbfbe198e3a16631a4
Zihan Hu <hu-zihan@foxmail.com>
2023-06-10 10:36:06 +0800

    fix(blog): 修复 sidebar 等错误

--- blog/.vuepress/theme.ts
+++ blog/.vuepress/theme.ts
@@ -1,17 +1,14 @@
 import { hopeTheme } from 'vuepress-theme-hope';
 import navbar from './navbar.js';
-import sidebar from './sidebar.js';
 
 export default hopeTheme({
   hostname: 'https://github.io/zihanhu/blog',
   navbar,
-  sidebar,
+  sidebar: false,
   editLink: false,
   blog: {
-    avatar: '',
     name: '胡梓晗',
     description: '全栈业余编程爱好者',
-    intro: '/me.html',
     medias: {
       Email: 'mailto:zihan-hu@foxmail.com',
       GitHub: 'https://github.com/ZihanHu',
@@ -36,4 +33,6 @@ export default hopeTheme({
   },
   pageInfo: ['Date', 'Category', 'Tag'],
   copyright: 'Under CC-BY-SA-4.0 | Copyright © 2023 Zihan Hu',
+  contributors: false,
+  lastUpdated: false,
 });
