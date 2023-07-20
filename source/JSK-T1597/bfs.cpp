506864173b039e569285bde3e4ff03c3fa785dc1
Zihan Hu <hu-zihan@foxmail.com>
2023-07-20 21:30:11 +0800

    chore(blog): 更新文章

---
+++ source/JSK-T1597/bfs.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Point {
+  Point(): Point(0, 0) {};
+  Point(int x, int y): x(x), y(y) {};
+  bool operator==(Point& p) const { return this->x == p.x && this->y == p.y; }
+  int x, y;
+};
+
+
+const int N = 20;
+int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+bool blocked[N][N], vis[N][N];
+int n, m, ans = INT32_MAX;
+queue<Point> q;
+
+inline bool valid(Point p) {
+  return !(p.x < 1 || p.x > m || p.y < 1 || p.y > n || blocked[p.x][p.y] || vis[p.x][p.y]);
+}
+inline bool win(Point p) {
+  return p.x == 1 || p.x == n || p.y == 1 || p.y == m;
+}
+
+/**
+ * @param p 搜索起点
+ * @param step 当前已走步数
+ */
+void dfs(Point p, int step) {
+  if (step > ans) return; // 已不能更优，剪枝
+  if (win(p)) { // 因为已进行最优性剪枝，确保了答案更优
+    ans = step;
+    return;
+  }
+  for (int i = 0; i < 4; ++i) {
+    Point u(p.x + dx[i], p.y + dy[i]);
+    if (valid(u)) {
+      vis[u.x][u.y] = 1;
+      dfs(u, step + 1);
+      vis[u.x][u.y] = 0;
+    }
+  }
+}
+
+int main() {
+  Point start;
+  cin >> n >> m;
+  char c;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      cin >> c;
+      switch (c) {
+        case '#': blocked[i][j] = 1; break;
+        case '@': start = Point(i, j); break;
+      }
+    }
+  }
+  dfs(start, 0);
+  cout << ((ans == INT32_MAX) ? -1 : ans); // @attention 这里必须加括号，否则会 WA
+  return 0;
+}
