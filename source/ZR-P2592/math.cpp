10a5d2d017eb2576c841cd5cc9d28751198aa258
Zihan Hu <hu-zihan@foxmail.com>
2023-07-26 23:55:04 +0800

    chore(blog): 更新文章

---
+++ source/ZR-P2592/math.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+typedef long long LL;
+
+const int dx[] = {1, 1, 2, 2, -1, -1, -2, -2}, dy[] = {2, -2, 1, -1, 2, -2, 1, -1};
+
+inline bool valid(LL x, LL y) {
+  return (x >= 1 && y >= 1);
+}
+
+int main() {
+  cin.sync_with_stdio(0); cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    LL x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    bool ok = 0;
+    for (int i = 0; i < 8; ++i) {
+      LL x = x1 + dx[i], y = y1 + dy[i];
+      if (valid(x, y) && x + y != x2 + y2 && abs(x2 - x) != abs(y2 - y)) {
+        ok = 1;
+        break;
+      }
+    }
+    cout << ok << endl;
+  }
+  return 0;
+}
