2361da077b443edcce7abbddba13f68558d7a72c
Zihan Hu <hu-zihan@foxmail.com>
2023-07-27 23:59:46 +0800

    chore(blog): 更新文章

---
+++ source/luogu-P1996/simulate.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+list<int> game;
+
+int main() {
+  cin.sync_with_stdio(0); cin.tie(0);
+  int n, m;
+  cin >> n >> m;
+  for (int i = 1; i <= n; ++i) game.push_back(i);
+  auto cur = game.begin();
+  int i = 0;
+  while (game.size()) {
+    ++i;
+    if (i == m) {
+      auto d = cur;
+      ++cur;
+      cout << *d << ' ';
+      game.erase(d);
+      i = 0;
+    } else {
+      ++cur;
+    }
+    if (cur == game.end()) cur = game.begin();
+  }
+  return 0;
+}
