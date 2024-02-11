98a17f7caab1407e911d371be32bc7533ab44900
Zihan Hu <hu-zihan@foxmail.com>
2024-02-12 01:01:08 +0800

    fix(blog): 修复导航栏、文章列表页显示错误

--- blog/.vuepress/navbar.ts
+++ blog/.vuepress/navbar.ts
@@ -2,7 +2,7 @@ import { navbar } from 'vuepress-theme-hope'
 
 export default navbar([
   { text: '主页', icon: 'home', link: '/' },
-  { text: '文章', icon: 'blog', link: '/article' },
-  { text: '分类', icon: 'categoryselected', link: '/category' },
-  { text: '标签', icon: 'tag', link: '/tag' },
+  { text: '文章', icon: 'blog', link: '/article/' },
+  { text: '分类', icon: 'categoryselected', link: '/category/' },
+  { text: '标签', icon: 'tag', link: '/tag/' },
 ])
